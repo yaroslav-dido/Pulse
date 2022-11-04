7820c4ee4c5cc41bedf94b6a878223948642896f
yarickdid <yarickdid090@gmail.com>
2022-11-04 18:55:32 +0200

    added carousel and adaptived

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// $(document).ready(function(){
+//   $('.carousel__inner').slick({
+//     speed: 1200,
+//     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left_arrow.png"></button>',
+//     nextArrow: '<button type="button" class="slick-next"><img src="../icons/right_arrow.png"></button>',
+//     responsive: [
+//         {
+//           breakpoint: 992,
+//           settings: {
+//             dots: true,
+//             arrows: false
+//          }
+//       }
+//     ]
+//   });
+// });
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false, 
+    controls: false,   
+    nav: false,
+    responsive: {
+      380: {
+        nav: true,
+
+      },
+      700: {
+        nav: true,
+
+      },
+    }
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+})
